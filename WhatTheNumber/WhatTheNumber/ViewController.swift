6e6e269c6c096d757efeabee6510aa24111784e3
Leo Chen <unision@gmail.com>
2017-08-10 23:03:27 +0800

    //輸入文字後，取代原輸入文字（可是只有第一次執行有效）哭臉 inputTextField.clearsOnInsertion = true

--- WhatTheNumber/WhatTheNumber/ViewController.swift
+++ WhatTheNumber/WhatTheNumber/ViewController.swift
@@ -1,25 +1,97 @@
-//
-//  ViewController.swift
-//  WhatTheNumber
-//
-//  Created by leo_unision on 2017/8/8.
-//  Copyright © 2017年 unision. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var btnText: UIButton!
+    
+    //設定變數的最大及最小值
+    var maxNumber = 100
+    var minNumber = 1
+    //設定變數，遊戲正在進行中
+    var isOver = false
+    
+    //取得1-100的亂數，並轉型為實數
+    var answer = Int(arc4random_uniform(100) + 1)
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //畫面載入時，文字輸入鍵盤就自動升起
+        inputTextField.becomeFirstResponder()
     }
+    
+    //MARK: Action Button
+    
+    @IBAction func btnGuess(_ sender: UIButton)
+    {
+        if isOver == false
+        {
+            //Playing Game
+            print("answer = \(answer)")
+            //取得文字輸入框的文字，並強制拆解（就是一定有有文字），存到常數inputText內
+            let inputText = inputTextField.text!
+            
+            //輸入完文字框後，自動清空文字框
+//            inputTextField.text = ""
+            
+            //take input text out
+            //print("inputText = \(inputText)")
+            let inputNumber = Int(inputText)
+            print("inputNumber = \(String(describing: inputNumber))")
+            
+            if inputNumber == nil
+            {
+                //no input
+                print("no input")
+                lblMessage.text = "尚未輸入！ 正確的數字介於 \(minNumber) ~ \(maxNumber)"
+            } else {
+                if inputNumber! > maxNumber {
+                    //user input too large
+                    print("too large")
+                    lblMessage.text = "太大了！正確的數字介於 \(minNumber) ~ \(maxNumber)"
+                } else if inputNumber! < minNumber {
+                    //user input too small
+                    print("too small")
+                    lblMessage.text = "太小了！正確的數字介於 \(minNumber) ~ \(maxNumber)"
+                } else {
+                    //check answer
+                    print("check answer")
+                    if inputNumber == answer {
+                        //Bingo! right anwer
+                        lblMessage.text = "好棒棒！！"
+                        inputTextField.text = "O"
+                        btnText.setTitle("再玩一次", for: .normal)
+                        isOver = true
+                    } else {
+                        //wrong answer
+                        if inputNumber! > answer {
+                            //large than answer
+                            maxNumber = inputNumber!
+                        } else {
+                            //small than answer
+                            minNumber = inputNumber!
+                        }
+                        lblMessage.text = "正確的數字介於 \(minNumber) ~ \(maxNumber)"
+                        inputTextField.text = "X"
+                        btnText.setTitle("再試一次", for: .normal)
+                        
+                        //輸入文字後，取代原輸入文字（第一次有效）
+                        inputTextField.clearsOnInsertion = true
+                    }
+                }
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        } else {
+            //Game is Over
+            maxNumber = 100
+            minNumber = 1
+            lblMessage.text = "正確的數字介於 \(minNumber) ~ \(maxNumber)"
+            answer = Int(arc4random_uniform(100) + 1)
+            isOver = false
+        }
     }
-
-
 }
 
